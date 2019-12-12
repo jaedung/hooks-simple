35bf57a73d5e84b2b4a8d1c00f8020b3b4b00295
justin <justinkim0310@gmail.com>
2019-12-12 11:53:57 +1000

    created useResources hookgp

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
 
   const fetchResource = async () => {
@@ -15,6 +15,12 @@ const ResourceList = ({ resource }) => {
   useEffect(() => {
     fetchResource();
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   return (
     <ul>
